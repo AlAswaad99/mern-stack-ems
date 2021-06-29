0ae7fdcb6ac9753a29511cef6a6133e45dc51d4d
Danny Terefe <dannyboy9917@gmail.com>
2021-06-30 02:58:37 +0300

    edit package.json include production build

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const EmployeeRoute = require("./routes/employee");
+const patj = require("path")
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -23,6 +24,13 @@ mongoose.connect(process.env.MONGODB_URI ||
 app.use(express.json());
 app.use("/employee", EmployeeRoute);
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
 
