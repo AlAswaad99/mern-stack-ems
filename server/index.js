1a50306b8a7a862b3baff012221d2ea7f78eeb23
Danny Terefe <dannyboy9917@gmail.com>
2021-07-21 15:37:02 +0300

    scroll-to-top on edit for mobile devices

--- server/index.js
+++ server/index.js
@@ -70,12 +70,12 @@ if(process.env.NODE_ENV === 'production'){
 
 // console.log(process.env.NODE_ENV);
 
-if(process.env.NODE_ENV === 'dev'){
-  app.use(express.static("../client/build"));
+// if(process.env.NODE_ENV === 'dev'){
+//   app.use(express.static("../client/build"));
 
-  // app.get("*", (req, res) =>{
-  //   res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-  // })
-}
+//   // app.get("*", (req, res) =>{
+//   //   res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+//   // })
+// }
 app.listen(port, ()=> console.log(`listening on port ${port}`));
 
